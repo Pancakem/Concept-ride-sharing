399bd2a7131bd27280d576eab439ab5df1c0f7e2
Pancakem <oumamarvin@gmail.com>
2018-12-18 21:45:18 +0300

    fixed endless loop

--- v1/src/pkg/ride/client.go
+++ v1/src/pkg/ride/client.go
@@ -38,76 +38,72 @@ func (c *Client) Send() {
 }
 
 func (c *Client) Read(rid chan *store.MatchResponse, an chan map[string]interface{}) {
+
+	c.conn.SetReadLimit(maxMessageSize)
+
 	for {
-		c.conn.SetReadLimit(maxMessageSize)
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			// do something clever
+			break
+		}
 
-		for {
-			_, message, err := c.conn.ReadMessage()
+		ma := make(map[string]interface{})
+		json.Unmarshal(message, &ma)
+
+		switch ma["type"] {
+		case "driverlocation":
+			if c.busy {
+				an <- ma
+			} else {
+				dl := &store.DriverLocation{}
+				json.Unmarshal(message, dl)
+				saveToDB(dl)
+			}
+
+		case "accepted":
+			// when a ride is accepted
+			// the driver details are found out
+			// and his/her location
+			// this data is sent to the rider channel which
+			// will trigger the send response to them
+			acc := &store.Accepted{}
+			json.Unmarshal(message, acc)
+			d := &model.Driver{ID: acc.DriverID}
+			err := d.GetByID()
 			if err != nil {
-				// do something clever
-				break
+				log.Println((err))
+			}
+			err = d.Vehicle.Get()
+			if err != nil {
+				log.Println((err))
 			}
 
-			ma := make(map[string]interface{})
-			json.Unmarshal(message, &ma)
-
-			switch ma["type"] {
-			case "driverlocation":
-				if c.busy {
-					an <- ma
-				} else {
-					dl := &store.DriverLocation{}
-					json.Unmarshal(message, dl)
-					saveToDB(dl)
-				}
-
-			case "accepted":
-				// when a ride is accepted
-				// the driver details are found out
-				// and his/her location
-				// this data is sent to the rider channel which
-				// will trigger the send response to them
-				acc := &store.Accepted{}
-				json.Unmarshal(message, acc)
-				d := &model.Driver{ID: acc.DriverID}
-				err := d.GetByID()
-				if err != nil {
-					log.Println((err))
-				}
-				err = d.Vehicle.Get()
-				if err != nil {
-					log.Println((err))
-				}
-
-				rid <- &store.MatchResponse{
-					Type:         "accepted",
-					LatLng:       acc.Location,
-					Name:         d.FullName,
-					PhoneNumber:  d.Phonenumber,
-					ImageURL:     d.ProfileImage,
-					VehicleColor: d.Vehicle.Color,
-					VehicleModel: d.Vehicle.Model,
-					VehiclePlate: d.Vehicle.PlateNumber,
-				}
-			case "cancelled":
-				// cancelled should contain ride id
-				finished("cancelled", ma["id"].(string), ma["time"].(float64), ma["distance"].(float64), an, c, false)
-				c.busy = false
-				break
-				return
-
-			case "finished":
-				finished("finished", ma["id"].(string), ma["time"].(float64), ma["distance"].(float64), an, c, true)
-				c.busy = false
-				break
-				return
-
-			case "rating":
-				store.AddRiderRating(ma["id"].(string), ma["rating"].(float32))
-
+			rid <- &store.MatchResponse{
+				Type:         "accepted",
+				LatLng:       acc.Location,
+				Name:         d.FullName,
+				PhoneNumber:  d.Phonenumber,
+				ImageURL:     d.ProfileImage,
+				VehicleColor: d.Vehicle.Color,
+				VehicleModel: d.Vehicle.Model,
+				VehiclePlate: d.Vehicle.PlateNumber,
 			}
+		case "cancelled":
+			// cancelled should contain ride id
+			finished("cancelled", ma["id"].(string), ma["time"].(float64), ma["distance"].(float64), an, c, false)
+			c.busy = false
+			return
+
+		case "finished":
+			finished("finished", ma["id"].(string), ma["time"].(float64), ma["distance"].(float64), an, c, true)
+			c.busy = false
+			return
+
+		case "rating":
+			store.AddRiderRating(ma["id"].(string), ma["rating"].(float32))
 
 		}
-	}
 
+	}
 }
