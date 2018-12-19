e8ccd0069b77aa913d0b82c94099d6ae2c09c36b
Pancakem <pancakesdeath@protonmail.com>
2018-12-19 11:57:33 +0300

    working demo

--- v1/src/pkg/store/db.go
+++ v1/src/pkg/store/db.go
@@ -21,6 +21,38 @@ type config struct {
 	sslmode  string `yaml:"sslmode"`
 }
 
+var schema = `
+CREATE TABLE request (  
+    id VARCHAR (50) NOT NULL PRIMARY KEY,
+    driverid VARCHAR (50) NOT NULL ,
+    riderid VARCHAR (50) NOT NULL,
+    actual_price REAL,
+    completed BOOLEAN NOT NULL,
+    ride_time VARCHAR (50),
+    distance REAL,
+    average_time VARCHAR (50), 
+    create_date VARCHAR (50)
+);
+
+CREATE TABLE place (
+    id VARCHAR NOT NULL PRIMARY KEY ,
+    origin_name VARCHAR (50),
+    origin_latitude REAL,
+    origin_longitude REAL,
+    destination_name VARCHAR (50),
+    destination_latitude REAL,
+    destination_longitude REAL,
+    
+    FOREIGN KEY (id) REFERENCES request (id)
+); 
+
+CREATE TABLE ratings (
+    rideid VARCHAR (50) NOT NULL,
+    riderrating REAL,
+    driverrating REAL
+);
+`
+
 var db *sql.DB
 
 func init() {
@@ -35,6 +67,15 @@ func init() {
 		log.Fatal("Couldn't establish database connection:", err)
 	}
 	db = d
+	createSchema()
+}
+
+func createSchema() {
+	_, err := db.Exec(schema)
+	if err != nil {
+		log.Println(err)
+	}
+
 }
 
 func getConfig() *config {
