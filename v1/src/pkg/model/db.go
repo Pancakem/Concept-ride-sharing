39f56d0f377e23a53773dd7ad2e41f41436524f8
Pancakem <pancakesdeath@protonmail.com>
2019-02-23 08:19:30 +0000

    add sql statements

--- v1/src/pkg/model/db.go
+++ v1/src/pkg/model/db.go
@@ -9,38 +9,6 @@ import (
 	"github.com/pancakem/rides/v1/src/pkg/setting"
 )
 
-var sqldb *sql.DB
-
-func setSQLEngine() {
-	conf := setting.Config{}
-	conf.Get()
-	dataSourceName := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
-		conf.Host, conf.Port, conf.User, conf.Password, conf.DatabaseName, conf.SSLMode)
-
-	toDb, err := sql.Open("postgres", dataSourceName)
-	if err != nil {
-		log.Println("Failed to connect to database", err)
-	}
-	sqldb = toDb
-	_, err = sqldb.Exec(schema)
-	if err != nil {
-		log.Println(err)
-	}
-
-}
-
-func init() {
-	setSQLEngine()
-}
-
-// DefaultService the CRUD
-type DefaultService interface {
-	Create() error
-	Get() error
-	Update() error
-	Delete() error
-	GetAll() interface{}
-}
 
 var schema = `
 CREATE TABLE rider (
@@ -86,3 +54,45 @@ CREATE TABLE blacklisted (
     token VARCHAR NOT NULL
 );
 `
+
+var sqldb *sql.DB
+
+func setSQLEngine() {
+	conf := setting.Config{}
+	conf.Get()
+	dataSourceName := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		conf.Host, conf.Port, conf.User, conf.Password, conf.DatabaseName, conf.SSLMode)
+
+	toDb, err := sql.Open("postgres", dataSourceName)
+	if err != nil {
+		log.Println("Failed to connect to database", err)
+	}
+	sqldb = toDb
+	_, err = sqldb.Exec(schema)
+	if err != nil {
+		log.Println(err)
+	}
+
+}
+
+func init() {
+	setSQLEngine()
+}
+
+// DefaultService the CRUD
+type DefaultService interface {
+	Create() error
+	Get() error
+	Update() error
+	Delete() error
+	GetAll() interface{}
+}
+
+// Check if the type exists
+func Exist(u DefaultService) bool {
+	err := u.Get()
+	if err != nil {
+		return false
+	}
+	return true
+}
