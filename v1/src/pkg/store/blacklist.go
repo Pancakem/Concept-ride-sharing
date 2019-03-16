b79cc15cda706535ec727f3ef01c128711a34abf
Pancakem <pancakesdeath@protonmail.com>
2019-03-16 23:40:28 +0300

    move blacklist to store

---
+++ v1/src/pkg/store/blacklist.go
@@ -0,0 +1,27 @@
+package store
+
+import (
+	"database/sql"
+)
+
+// BlackListed tokens
+type BlackListed struct {
+	Token string `bson:"token"`
+}
+
+// Create adds a used or expired token to db so that it can't be misused
+func (bl *BlackListed) Create() error {
+	_, err := db.Exec("INSERT * INTO blacklisted VALUES(?)", bl.Token)
+	return err
+}
+
+// GetToken checks for the token if exists in the database
+func GetToken(token string) bool {
+	row := db.QueryRow("SELECT * FROM blacklisted where token=?", token)
+	var toke string
+	err := row.Scan(&toke)
+	if err == sql.ErrNoRows || err != nil {
+		return false
+	}
+	return true
+}
