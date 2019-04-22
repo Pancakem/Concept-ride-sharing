616b75d03391ece70bed13a223beda84c881c2e1
Pancakem <pancakesdeath@protonmail.com>
2019-04-22 16:48:23 +0300

    remove unnecessary globals

--- v1/src/cmd/app/server.go
+++ v1/src/cmd/app/server.go
@@ -8,11 +8,11 @@ import (
 	"github.com/pancakem/rides/v1/src/pkg/route"
 )
 
-var addr = flag.String("addr", "0.0.0.0:4000", "http service address")
-
 func main() {
+	var addr = flag.String("addr", "0.0.0.0:4000", "http service address")
 	flag.Parse()
 	router := route.InitRouter()
+
 	log.Println("Listening at port 4000")
 	log.Fatal(http.ListenAndServe(*addr, router))
 }
