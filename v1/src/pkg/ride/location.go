410de73aae31e335f8897041be644e943ec4a7cb
Pancakem <pancakesdeath@protonmail.com>
2019-04-22 13:33:40 +0300

    format change

--- v1/src/pkg/ride/location.go
+++ v1/src/pkg/ride/location.go
@@ -18,7 +18,6 @@ func GetLocation(hub *Hub, w http.ResponseWriter, r *http.Request) {
 
 	client.conn.ReadJSON(dl)
 
-
 	rediscli := store.GetRedisClient()
 	rediscli.AddDriverLocation(dl)
 	a := aggreg{id: dl.DriverID, Client: client}
